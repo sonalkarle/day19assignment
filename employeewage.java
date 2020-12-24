ae2a20af31246b3581663df6fa23ccdbb165c7be
sonal <sonalkarle01@gmail.com>
2020-12-25 00:19:42 +0530

    Solving using Switch case

--- employeewage.java
+++ employeewage.java
@@ -1,7 +1,33 @@
-class employeewage
+public class employeewage
 {
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+	public static final int EMP_RATE_PER_HR=20;
+
+
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee wage computation");
+		//variable
+		int empHr=0;
+		int salary=0;
+		//Computation
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+
+			case FULL_TIME:
+			System.out.println("Employee is fulltime");
+			empHr=8;
+			break;
+			case PART_TIME:
+			System.out.println("Employee is parttime");
+			empHr=4;
+			break;
+			default:
+			System.out.println("Employee is absent");
+			empHr=0;
+
+		}
+		salary=(empHr*EMP_RATE_PER_HR);
+		System.out.println("Salary is "+salary);
 	}
 }
