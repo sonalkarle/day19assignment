1e5b0a0953473f81e160b3594e349e5bb561409f
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-25 02:19:17 +0530

    Update employeewage.java

--- employeewage.java
+++ employeewage.java
@@ -1,5 +1,6 @@
 public class employeewage
 {
+	//Calculating wages for month
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
