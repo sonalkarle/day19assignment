33a6335b5bdac0ddea9f16086e0c35ffad10d4c5
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-25 02:05:25 +0530

    Update employeewage.java

--- employeewage.java
+++ employeewage.java
@@ -2,12 +2,16 @@ class employeewage
 {
 	public static void main(String[] args)
 	{
+		//Add part time employee and wages
+		//variable
 		int isfulltime=1;
 		int isparttime=2;
 		int empRatePerHr=20;
 		int empHr=0;
 		int salary=0;
+		//computation
 		int res=(int)(Math.random()*10)%3;
+		//computation
 		if(res==isfulltime)
 		{
 			System.out.println("Employee is fulltime");
@@ -23,6 +27,7 @@ class employeewage
 			System.out.println("Employee is absent");
 			empHr=0;
 		}
+		//computation
 		salary=(empHr*empRatePerHr);
 		System.out.println("Salary is "+salary);
 	}
