96fc40b9e001da0f8c7c54dea788ccccc4db8741
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-25 02:01:54 +0530

    Update employeewage.java

--- employeewage.java
+++ employeewage.java
@@ -2,11 +2,15 @@ class employeewage
 {
 	public static void main(String[] args)
 	{
+		//Calculate daily wages of employee
+		//variables
 		int isPresent=1;
 		int empRatePerHr=20;
 		int salary=0;
 		int empHr=0;
+		//computation
 		int res=(int)(Math.random()*10)%2;
+		//computation
 		if(res==isPresent)
 		{
 			System.out.println("Employee is present");
@@ -17,6 +21,7 @@ class employeewage
 			System.out.println("Employee is absent");
 			empHr=0;
 		}
+		//computation
 		salary=(empRatePerHr*empHr);
 		System.out.println("Salary is " +salary);
 	}
