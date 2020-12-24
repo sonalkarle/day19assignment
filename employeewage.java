776955ddec86cac39ca2acd37a88079599c14894
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-25 02:13:59 +0530

    Update employeewage.java

--- employeewage.java
+++ employeewage.java
@@ -1,5 +1,7 @@
 public class employeewage
 {
+	//solving using switch cases
+
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
