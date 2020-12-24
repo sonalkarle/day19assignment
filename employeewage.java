2172a67d0c1578ffa9390ae49e390d52f4a8da1f
sonal <sonalkarle01@gmail.com>
2020-12-24 23:43:31 +0530

    Add part time employee and wages

--- employeewage.java
+++ employeewage.java
@@ -2,6 +2,28 @@ class employeewage
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee wage computation");
+		int isfulltime=1;
+		int isparttime=2;
+		int empRatePerHr=20;
+		int empHr=0;
+		int salary=0;
+		int res=(int)(Math.random()*10)%3;
+		if(res==isfulltime)
+		{
+			System.out.println("Employee is fulltime");
+			empHr=8;
+		}
+		else if(res==isparttime)
+		{
+			System.out.println("Employee is Parttime");
+			empHr=4;
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+			empHr=0;
+		}
+		salary=(empHr*empRatePerHr);
+		System.out.println("Salary is "+salary);
 	}
 }
