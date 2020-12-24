b89488724e88bb0fd8daec420210fcfa3699230c
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-25 01:56:44 +0530

    Update employeewage.java

--- employeewage.java
+++ employeewage.java
@@ -2,8 +2,11 @@ class employeewage
 {
 	public static void main(String[] args)
 	{
+		//variable
 		int isPresent=1;
+		//computation
 		int res=(int)(Math.random()*10)%2;
+		//computation
 		if(res==isPresent)
 		{
 			System.out.println("Employee is present");
